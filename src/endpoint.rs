bf365aea3c545f42bfafa401c81f53de9b7ccc24
Michal 'vorner' Vaner <vorner@vorner.cz>
2017-02-18 20:46:18 +0100

    Batch processing
    Turn a batch into bunch of futures and one future to put it all
    together. A bit convoluted concept (and a bit more convoluted
    implementation), ideas for simplification are welcome.
    
    Hopefully the rust's strict type system keeps this from falling apart.

--- src/endpoint.rs
+++ src/endpoint.rs
@@ -12,7 +12,7 @@
 //! [`EmptyServer`](struct.EmptyServer.html) as the server. If you want a server-only endpoint,
 //! simply don't call any RPCs or notifications.
 
-use message::{Message, Parsed, Response, Request, Notification};
+use message::{Broken, Message, Parsed, Response, Request, Notification};
 
 use std::io::{Error as IoError, ErrorKind};
 use std::collections::HashMap;
@@ -79,6 +79,7 @@ pub trait Server {
 type BoxFuture<T, E> = Box<Future<Item = T, Error = E>>;
 type FutureMessage = BoxFuture<Option<Message>, IoError>;
 type BoxStream<T, E> = Box<Stream<Item = T, Error = E>>;
+type FutureMessageStream = BoxStream<FutureMessage, IoError>;
 
 // A future::stream::once that takes only the success value, for convenience.
 fn once<T, E>(item: T) -> Once<T, E> {
@@ -104,6 +105,81 @@ fn do_notification<RPCServer: Server>(server: &RPCServer, notification: Notifica
     }
 }
 
+// To process a batch using the same set of parallel executors as the whole server, we produce a
+// stream of the computations which return nothing, but gather the results. Then we add yet another
+// future at the end of that stream that takes the gathered results and wraps them into the real
+// message ‒ the result of the whole batch.
+fn do_batch<RPCServer: Server + 'static>(server: &RPCServer, msg: Vec<Message>) -> FutureMessageStream {
+    // Create a large enough channel. We may be unable to pick up the results until the final
+    // future gets its turn, so shorter one could lead to a deadlock.
+    let (sender, receiver) = channel(msg.len());
+    // Each message produces a single stream of futures. Create that streams (right now, so we
+    // don't have to keep server long into the future).
+    let small_streams: Vec<_> = msg.into_iter()
+        .map(|sub| -> Result<_, IoError> {
+            let sender = sender.clone();
+            // This part is a bit convoluted. The do_msg returns a stream of futures. We want to
+            // take each of these futures (the outer and_then), run it to completion (the inner
+            // and_then), send its result through the sender if it provided one and then
+            // convert the result to None.
+            //
+            // Note that do_msg may return arbitrary number of work futures, but only at most one
+            // of them is supposed to provide a resulting value which would be sent through the
+            // channel. Unfortunately, there's no way to know which one it'll be, so we have to
+            // clone the sender all over the place.
+            //
+            // Also, it is a bit unfortunate how we need to allocate so many times here. We may try
+            // doing something about that in the future, but without implementing custom future and
+            // stream types, this seems the best we can do.
+            let all_sent = do_msg(server, Ok(sub)).and_then(move |future_message| -> Result<FutureMessage, _> {
+                let sender = sender.clone();
+                let msg_sent = future_message.and_then(move |response: Option<Message>| -> FutureMessage {
+                    match response {
+                        None => Box::new(Ok(None).into_future()),
+                        Some(msg) => {
+                            Box::new(sender.send(msg)
+                                .map_err(|_| IoError::new(ErrorKind::Other, "Shouldn't happen"))
+                                .map(|_| None))
+                        },
+                    }
+                });
+                Ok(Box::new(msg_sent))
+            });
+            Ok(all_sent)
+        })
+        .collect();
+    // We make it into a stream of streams and flatten it to get one big stream
+    let subs_stream = stream::iter(small_streams).flatten();
+    // Once all the results are produced, wrap them into a batch and return that one
+    let collected = receiver.collect()
+        .map_err(|_| IoError::new(ErrorKind::Other, "Shouldn't happen"))
+        .map(|results| if results.is_empty() {
+            // The spec says to send nothing at all if there are no results
+            None
+        } else {
+            Some(Message::Batch(results))
+        });
+    let streamed: Once<FutureMessage, _> = once(Box::new(collected));
+    // Connect the wrapping single-item stream after the work stream
+    Box::new(subs_stream.chain(streamed))
+}
+
+// Handle single message and turn it into an arbitrary number of futures that may be worked on in
+// parallel, but only at most one of which returns a response message
+fn do_msg<RPCServer: Server + 'static>(server: &RPCServer, msg: Parsed) -> FutureMessageStream {
+    match msg {
+        Err(broken) => {
+            let err: FutureMessage = Ok(Some(broken.reply())).into_future().boxed();
+            Box::new(once(err))
+        },
+        Ok(Message::Request(req)) => Box::new(once(do_request(server, req))),
+        Ok(Message::Notification(notif)) => Box::new(once(do_notification(server, notif))),
+        Ok(Message::Batch(batch)) => do_batch(server, batch),
+        Ok(Message::UnmatchedSub(value)) => do_msg(server, Err(Broken::Unmatched(value))),
+        _ => Box::new(empty()),
+    }
+}
+
 /// A RPC server that knows no methods
 ///
 /// You can use this if you want to have a client-only [Endpoint](struct.Endpoint.html). It simply
@@ -119,18 +195,6 @@ impl Server for EmptyServer {
 pub struct Client {
     idmap: Rc<HashMap<String, RelaySender<Response>>>,
     sender: Sender<Message>,
-}
-
-fn msg_handle<RPCServer: Server + 'static>(server: &RPCServer, msg: Parsed) -> BoxStream<FutureMessage, IoError> {
-    match msg {
-        Err(broken) => {
-            let err: FutureMessage = Ok(Some(broken.reply())).into_future().boxed();
-            Box::new(once(err))
-        },
-        Ok(Message::Request(req)) => Box::new(once(do_request(server, req))),
-        Ok(Message::Notification(notif)) => Box::new(once(do_notification(server, notif))),
-        _ => Box::new(empty()),
-    }
 }
 
 // TODO: Some other interface to this?
@@ -149,7 +213,7 @@ pub fn endpoint<Connection, RPCServer>(handle: Handle, connection: Connection, s
     // parallel.
 
     // TODO: Have a concrete enum-type for the futures so we don't have to allocate and box it.
-    let answers = stream.map(move |parsed| msg_handle(&server, parsed))
+    let answers = stream.map(move |parsed| do_msg(&server, parsed))
         .flatten()
         .buffer_unordered(4)
         .filter_map(|message| message);
