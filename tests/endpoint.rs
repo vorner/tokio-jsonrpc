18c11217b09cee4cab7940fcd7dde92cbf4e5d80
Michal 'vorner' Vaner <vorner@vorner.cz>
2017-02-26 11:42:06 +0100

    A test for sending a notification

--- tests/endpoint.rs
+++ tests/endpoint.rs
@@ -26,7 +26,8 @@ struct AnswerServer;
 
 /// A test server
 ///
-/// It expects no parameters and the method to be `"test"` and returns 42.
+/// It expects no parameters and the method to be `"test"` and returns 42 on RPC. It expects
+/// `"notif"` as a notification. Both `rpc` and `notification` terminate the server.
 impl Server for AnswerServer {
     type Success = u32;
     type RPCCallResult = Result<u32, ServerError>;
@@ -36,6 +37,12 @@ impl Server for AnswerServer {
         assert_eq!(method, "test");
         assert!(params.is_none());
         Some(Ok(42))
+    }
+    fn notification(&self, ctl: &ServerCtl, method: &str, params: &Option<Value>) -> Option<Self::NotificationResult> {
+        ctl.terminate();
+        assert_eq!(method, "notif");
+        assert!(params.is_none());
+        Some(Ok(()))
     }
 }
 
@@ -76,7 +83,7 @@ fn prepare() -> (Core, Framed<TcpStream, LineCodec>, Framed<TcpStream, LineCodec
 /// Run both the server and client, send a request and wait for the answer. The server terminates
 /// after the first request, so we can wait for both to terminate.
 #[test]
-fn answer() {
+fn rpc_answer() {
     let (mut reactor, s1, s2) = prepare();
     let both = {
         // Run in a sub-block, so we drop all the clients, etc.
@@ -86,6 +93,26 @@ fn answer() {
         let client_finished = client.call("test".to_owned(), None, None)
             .and_then(|(_client, answered)| answered)
             .map(|response| assert_eq!(json!(42), response.unwrap().result.unwrap()));
+        let both = server_finished.map_err(|_| IoError::new(ErrorKind::Other, "Canceled"))
+            .join(client_finished)
+            .map(|_| ())
+            .map_err(|e| panic!("{:?}", e));
+        both
+    };
+    reactor.run(both).unwrap();
+}
+
+/// Send a notification to the server.
+#[test]
+fn notification() {
+    let (mut reactor, s1, s2) = prepare();
+    let both = {
+        // Run in a sub-block, so we drop all the clients, etc.
+        let handle = reactor.handle();
+        let (_client, _ctl, server_finished) = Endpoint::new(s1, AnswerServer).start(&handle);
+        let (client, _ctl, _finished) = Endpoint::client_only(s2).start(&handle);
+        let client_finished = client.notify("notif".to_owned(), None)
+            .and_then(|_client| Ok(()));
         let both = server_finished.map_err(|_| IoError::new(ErrorKind::Other, "Canceled"))
             .join(client_finished)
             .map(|_| ())
